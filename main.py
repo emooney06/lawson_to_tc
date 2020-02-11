c1dcfd8f19e6fb4dd32bfdda50c08e85b1e67b08
Ethan Mooney <mooney.ethan@gmail.com>
2020-02-11 08:39:24 -0700

    function to convert html version of schedule to name, stime, etime for tc specs

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from pathlib import Path
 import os
 import pandas as pd
 from datetime import datetime
+from datetime import timedelta
 import lxml
 import html5lib
 from bs4 import BeautifulSoup
- 
-#from tabula import read_pdf
-#from tabula import convert_into
-#from tika import parser
+import numpy as np
  
 pd.option_context('display.max_rows', None, 'display.max_columns', None)  # more options can be specified also
 pd.options.display.max_colwidth = 199
@@ -16,16 +14,17 @@ pd.options.display.max_columns = 1000
 pd.options.display.width = 1000
 pd.options.display.precision = 2  # set as needed
  
-lawson_path = 'K:/ClinicalAdvisoryTeam/test_folder/4 South/feb_march_2020.htm'
- 
+on_laptop = True
+
+if on_laptop == True:
+    lawson_path = 'C:/Users/Ethan/OneDrive/TigerConnect_MPaCC/test_folder/4_south/feb_march_2020.htm'
+    lawson_out_file = 'C:/Users/Ethan/OneDrive/TigerConnect_MPaCC/test_folder/4_south/lawson_out.csv'
+    lawson_out_file2 = 'C:/Users/Ethan/OneDrive/TigerConnect_MPaCC/test_folder/4_south/lawson_out2.csv'
+else:
+    lawson_path = 'K:/ClinicalAdvisoryTeam/test_folder/4 South/feb_march_2020.htm'
+    lawson_out_file = 'K:/ClinicalAdvisoryTeam/test_folder/4 South/lawson_out.csv'
+
 lawson_df = pd.read_html(lawson_path)
- 
- 
-lawson_path2 = os.path.join('C:\\', 'Users', 'Ethan', 'OneDrive', 'Desktop', 'lawson_test.pdf')
-tmpTxtPath = os.path.join('C:\\', 'Users', 'Ethan', 'OneDrive', 'Desktop', 'tmpTxt.txt')
-tmpCSVPath = os.path.join('C:\\', 'Users', 'Ethan', 'OneDrive', 'Desktop', 'pdfToCSV.csv')
- 
-lawson_out_file = 'K:/ClinicalAdvisoryTeam/test_folder/4 South/lawson_out.csv'
  
 lawson_df = lawson_df[0]
  
@@ -57,17 +56,7 @@ lawson_stacked = pd.read_csv(lawson_out_file, names=['date', 'name', 'shift_code
 lawson_stacked = lawson_stacked[lawson_stacked.date != 'Staff ID']
 lawson_stacked['date'] = lawson_stacked['date'].str[3:]
  
- 
-####*************** seems good up to here *****************##########
- 
- 
-## remove rows for floor shifts (A1 is day, P1 is nights) this can happen with releifs and sups working floor shifts
-#lawson_stacked = lawson_stacked[lawson_stacked.shift_code != 'P1']
-#lawson_stacked = lawson_stacked[lawson_stacked.shift_code != 'A1']
- 
-## get rid of the '/r' that appears in the conversion from pdf with long naes
-#lawson_stacked['name'] = lawson_stacked['name'].str.replace('\r', ' ')
- 
+
 #add a column for the start times
 lawson_stacked.loc[lawson_stacked['shift_code'] == 'A0', 'start_time'] = '19:00:00'
 lawson_stacked.loc[lawson_stacked['shift_code'] == 'A4', 'start_time'] = '07:00:00'
@@ -78,52 +67,126 @@ lawson_stacked.loc[lawson_stacked['shift_code'] == 'A4', 'end_time'] = '19:00:00
 this_year = datetime.now().year
 #add a column for the year
 lawson_stacked['year_sched'] = this_year
-######################################################################
-''' Need to write some logic to address schedules at the end of the year, something like if current month 
-is Nov or Dec,then current year = year +1 '''
-######################################################################
- 
-#lawson_stacked = lawson_stacked[lawson_stacked.date != 'Staff ID']
- 
- 
+
 #split the month/day into separate columns
 lawson_stacked[['month_sched','day_sched']] = lawson_stacked['date'].str.split('/', expand=True)
-# this is a rediculous solution to make single digit months into 2 digit strings
-lawson_stacked['zero_col'] = '0'
  
 lawson_stacked = lawson_stacked.dropna()
- 
- 
-lawson_stacked['cal_day'] = lawson_stacked['year_sched'].astype(str) +'-' + lawson_stacked['month_sched'].astype(str) + '-' + lawson_stacked['day_sched'].astype(str)
-lawson_stacked['cal_day'] = pd.to_datetime(lawson_stacked['cal_day']).datetime64
- 
-lawson_stacked['day_plus'] = pd.Timestamp(lawson_stacked['cal_day']) + pd.DateOffset(days=1).strftime('%Y-%m-%d')
- 
-x = '2017-05-15'
-# choose some combination of below methods
-res = (pd.Timestamp(x) + pd.DateOffset(days=1)).strftime('%Y-%m-%d')
- 
- 
- 
+  
 # use a mask to identify months that are one character
 mask = lawson_stacked['month_sched'].str.len() == 1
-# use the mask to change single character months to 2 character months
-lawson_stacked.month_sched = lawson_stacked.month_sched.mask(mask, lawson_stacked['zero_col'] + lawson_stacked['month_sched'])
+lawson_stacked.month_sched = lawson_stacked.month_sched.mask(mask, '0' + lawson_stacked['month_sched'])
  
 mask = lawson_stacked['day_sched'].str.len() == 1
-lawson_stacked.day_sched = lawson_stacked.day_sched.mask(mask, lawson_stacked['zero_col'] + lawson_stacked['day_sched'])
- 
+lawson_stacked.day_sched = lawson_stacked.day_sched.mask(mask, '0' + lawson_stacked['day_sched'])
+
+
+
+lawson_stacked['cal_day'] = lawson_stacked['year_sched'].astype(str) +'-' + lawson_stacked['month_sched'].astype(str) + '-' + lawson_stacked['day_sched'].astype(str)
+lawson_stacked['cal_day'] = pd.to_datetime(lawson_stacked['cal_day'], yearfirst=True)
+
+lawson_stacked.to_csv(lawson_out_file)
+lawson = pd.read_csv(lawson_out_file)
+
+
+# Create a list to store the data
+next_days = []
+
+# For each row in the column,
+for row in lawson['shift_code']:
+    # if more than a value,
+    if row == 'A0':
+        next_days.append(1)
+    else:
+        # Append a failing grade
+        next_days.append(0)
+
+# Create a column from the list
+lawson['next_day'] = next_days
+lawson['cal_day'] = pd.to_datetime(lawson['cal_day'])
+
+temp = lawson['next_day'].apply(np.ceil).apply(lambda x: pd.Timedelta(x, unit='D'))
+
+lawson['shift_end'] = lawson['cal_day'] + temp
+
+lawson.to_csv(lawson_out_file)
  
 # combine columns to get the stime and etime as specified in tiger connect sample file
-lawson_stacked['stime'] = lawson_stacked['year_sched'].astype(str) + '-' + lawson_stacked['month_sched'].astype(str) + '-' + lawson_stacked['day_sched'].astype(str) + 'T' + lawson_stacked['start_time'].astype(str) + 'Z'
-lawson_stacked['etime'] = lawson_stacked['year_sched'].astype(str) + '-' + lawson_stacked['month_sched'].astype(str) + '-' + lawson_stacked['day_sched'].astype(str) + 'T' + lawson_stacked['end_time'].astype(str) + 'Z'
- 
- 
+lawson['stime'] = lawson['cal_day'].astype(str) + 'T' + lawson['start_time'].astype(str) + 'Z'
+lawson['etime'] = lawson['shift_end'].astype(str) + 'T' + lawson['end_time'].astype(str) + 'Z'
+
+lawson_for_schedule = lawson[['name','stime', 'etime']] 
+
 lawson_stacked = lawson_stacked.dropna()
- 
  
 # write the dataframe to a csv
 lawson_stacked.to_csv(lawson_out_file)
  
-lawson_stacked
+
+
+#lawson_stacked
+#def f(row):
+#    if row['shift_code'] == 'A0':
+#        val = pd.DatetimeIndex(lawson['cal_day']) + pd.DateOffset(1)
+#    else:
+#        val = lawson['cal_day']
+#    return val
+
+#lawson2 = lawson
+
+#lawson2.apply(f, axis=1)
+#lawson['shift_end'] = np.where(lawson['shift_code'] == 'A0', lawson['cal_day'] + pd.DateOffset(-1) , lawson['end_time'])
+
+#####################################################
+#pd.to_datetime('1970-01-01').value
+#lawson['shift_end2'] = pd.to_datetime(lawson['cal_day']).value
+#lawson['shift_end'] = pd.DatetimeIndex(lawson['cal_day']) + pd.DateOffset(-1)
+
+
+
+
+
+
+#lawson_stacked['cal_day'].dt(dayofweek)
+ 
+#lawson_stacked['day_plus'] = pd.Timestamp(lawson_stacked['cal_day']) + pd.DateOffset(days=1).strftime('%Y-%m-%d')
+ 
+#x = '2017-05-15'
+## choose some combination of below methods
+#res = (pd.Timestamp(x) + pd.DateOffset(days=1)).strftime('%Y-%m-%d')
+
+
+## For each row in the column,
+#for row in lawson['shift_code']:
+#    # if more than a value,
+#    if row == 'A0':
+#        # Append a letter grade
+#        n_day = (pd.DatetimeIndex(lawson['cal_day']) + pd.DateOffset(1))
+#        next_days.append(pd.DatetimeIndex(lawson['cal_day']) + pd.DateOffset(1))
+#        print('true')
+#        print(n_day)
+#    else:
+#        # Append a failing grade
+#        next_days.append(lawson['cal_day'])
+#        print('false')
+
+
+#df.loc[df['A'] == df['B'], 'C'] = 0
+#df.loc[df['A'] > df['B'], 'C'] = 1
+#df.loc[df['A'] < df['B'], 'C'] = -1
+##############################################################
+ 
+#lawson['shift_end'] = np.where(lawson.shift_code == 'A0', pd.DatetimeIndex(lawson['cal_day'] + pd.DateOffset(1), False))
+#lawson.loc[lawson['shift_end']] = lawson.shift_code.apply(lambda x: pd.DatetimeIndex(lawson2['cal_day']) + pd.DateOffset(1) if x == 'A0' else lawson_stacked['cal_day'])
+#gapminder.head()
+#lawson2.loc[lawson2['shift_code'] == 'A0', 'shift_end'] = pd.DatetimeIndex(lawson2['cal_day']) + pd.DateOffset(1) 
+#lawson_stacked.loc[lawson_stacked['shift_code'] == 'A4', 'shift_end'] = lawson_stacked['cal_day']
+
+
+## remove rows for floor shifts (A1 is day, P1 is nights) this can happen with releifs and sups working floor shifts
+#lawson_stacked = lawson_stacked[lawson_stacked.shift_code != 'P1']
+#lawson_stacked = lawson_stacked[lawson_stacked.shift_code != 'A1']
+ 
+## get rid of the '/r' that appears in the conversion from pdf with long naes
+#lawson_stacked['name'] = lawson_stacked['name'].str.replace('\r', ' ')
  
